011b708d7ba0debd3f0cddf19508d475c74fa5b1
Agricio <agricio.vitoretti@gmail.com>
2026-03-12 15:44:52 -0300

    Aprimorando Api com express para apresentar algumas info e tirar do arquivo index.html

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,33 @@
 const express = require("express");
 const cors = require("cors");
+const path =require("path");
 
 const app = express();
 
 app.use(cors());
+app.use(express.static(path.join(__dirname, "../frontend")));
 
-app.get("/api/mensagem", (req, res) => {
+app.get("/api/mensagem-api", (req, res) => {
   res.json({
     mensagem: "API funcionando 🚀"
   });
+});
+
+app.get("/api/servicos", (req, res) => {
+  res.json([
+    {
+      titulo: "Suporte técnico",
+      descricao: "Atendimento para resolução de problemas técnicos e manutenção de sistemas."
+    },
+    {
+      titulo: "Infraestrutura",
+      descricao: "Organização de redes, servidores e ambientes tecnológicos."
+    },
+    {
+      titulo: "Desenvolvimento Web",
+      descricao: "Criação de páginas institucionais e integração com APIs."
+    }
+  ]);
 });
 
 app.listen(3000, () => {
